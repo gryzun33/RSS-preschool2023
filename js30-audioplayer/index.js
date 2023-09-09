49f587a6410236eb9f2222e1db0e304510dbc8b2
gryzun33 <yakushevao777@gmail.com>
2023-09-09 18:06:38 +0300

    feat: add currentTime and fullTime

--- js30-audioplayer/index.js
+++ js30-audioplayer/index.js
@@ -10,6 +10,10 @@ const prevBtn = document.querySelector('.prev-track');
 const nextBtn = document.querySelector('.next-track');
 const pauseBtn = document.querySelector('.pause-track');
 
+const progressCurrent = document.querySelector('.progress-current');
+const progressToggler = document.querySelector('.progress-toggler');
+const currTimeHTML = document.querySelector('.current-time');
+
 const audios = [];
 tracksData.forEach((item) => {
   const audio = new Audio (item.link);
@@ -20,7 +24,27 @@ tracksData.forEach((item) => {
     audio.volume = 0.5;
     console.log(audio.paused);
   });
+  audio.addEventListener("timeupdate", () => {
+    const progressWidth = (audio.currentTime * 100) / audio.duration;
+    currTimeHTML.innerHTML = convertTime(audio.currentTime)
+    progressCurrent.style.width = `${progressWidth}%`;
+    progressToggler.style.left = `${progressWidth}%`;
+  });
+
+  audio.addEventListener("ended", () => {
+    audio.currentTime = 0;
+    progressCurrent.style.width = `0%`;
+    progressToggler.style.left = `0%`;
+    playNext();
+    // audios[currIndex+1].play();
+  });
+
 });
+
+setTimeout(() => {
+  renderCurrentAudio(audios[0]);
+}, 100);
+
 
 playBtn.addEventListener('click', () => {
   console.log('play');
@@ -28,6 +52,7 @@ playBtn.addEventListener('click', () => {
   audios[currIndex].play();
   playBtn.classList.add('hidden');
   pauseBtn.classList.remove('hidden');
+  // renderFullDuration(audios[currIndex]);
 });
 
 pauseBtn.addEventListener('click', () => {
@@ -40,22 +65,25 @@ pauseBtn.addEventListener('click', () => {
 
 prevBtn.addEventListener('click', () => {
   console.log('prev');
-  prevIndex = currIndex;
-  audios[prevIndex].pause();
-  audios[prevIndex].currentTime = 0;
-  currIndex = currIndex - 1;
-  if (currIndex < 0) {
-    currIndex = tracksData.length - 1;
-  }
-  if (isPlaying) {
-    audios[currIndex].play(); 
-  } else {
-    audios[currIndex].pause(); 
-  }
+  playPrev();
+  // renderFullDuration(audios[currIndex]);
 });
 
 nextBtn.addEventListener('click', () => {
   console.log('next');
+  playNext();
+  
+});
+
+function convertTime(duration) {
+  let min = Math.floor(duration / 60);
+  let sec = Math.floor(duration - min * 60);
+  min = min < 10 ? ('0' + min) : min; 
+  sec = sec < 10 ? ('0' + sec) : sec;
+  return `${min}:${sec}`;
+}
+
+function playNext() {
   prevIndex = currIndex;
   audios[prevIndex].pause();
   audios[prevIndex].currentTime = 0;
@@ -68,6 +96,38 @@ nextBtn.addEventListener('click', () => {
   } else {
     audios[currIndex].pause(); 
   }
-});
+  renderCurrentAudio(audios[currIndex]);
+}
 
+function playPrev() {
+  prevIndex = currIndex;
+  audios[prevIndex].pause();
+  audios[prevIndex].currentTime = 0;
+  currIndex = currIndex - 1;
+  if (currIndex < 0) {
+    currIndex = tracksData.length - 1;
+  }
+  if (isPlaying) {
+    audios[currIndex].play(); 
+  } else {
+    audios[currIndex].pause(); 
+  }
+  renderCurrentAudio(audios[currIndex]);
+}
+
+function renderCurrentAudio(audio) {
+  console.log ('render audio');
+  renderFullDuration(audio);
+  // renderName();
+}
+
+function renderFullDuration(audio) {
+  console.log('audio', audio); 
+  const fullTime = document.querySelector('.full-time');
+  fullTime.innerHTML = convertTime(audio.duration);
+}
+
+// function renderName() {
+//   const curr
+// }
 
