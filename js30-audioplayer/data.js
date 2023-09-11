1e28d53bc1021c47c686c0d9f4f0144ac500b9f7
gryzun33 <yakushevao777@gmail.com>
2023-09-12 01:37:38 +0300

    feat: change order of songs

--- js30-audioplayer/data.js
+++ js30-audioplayer/data.js
@@ -6,17 +6,12 @@ export const tracksData = [
     image: './assets/images/T-Rex.jpg'
   },
   {
-    link: './assets/audio/Love-Everybodys_Gotta_Live.mp3',
-    author: 'Love',
-    trackName: 'Everybody Gotta Live',
-    image: './assets/images/Love.jpg'
-  },
-  {
     link: './assets/audio/Ten_Years_After-Id_Love_to_Change_the_World.mp3',
     author: 'Ten Years After',
     trackName: "I'd Love to Change the World",
     image: './assets/images/Ten_Years_After.jpg'
   },
+ 
   {
     link: './assets/audio/Shocking_Blue-Daemon_Lover.mp3',
     author: 'Shocking Blue',
@@ -28,5 +23,11 @@ export const tracksData = [
     author: 'The Doors',
     trackName: "20th Century Fox",
     image: './assets/images/The_Doors.jpg'
-  }  
+  },
+  {
+    link: './assets/audio/Love-Everybodys_Gotta_Live.mp3',
+    author: 'Love',
+    trackName: 'Everybody Gotta Live',
+    image: './assets/images/Love.jpg'
+  } 
 ]
