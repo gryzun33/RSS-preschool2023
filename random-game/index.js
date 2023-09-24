878dcf655a16c787536b64865bd3efb71309b908
gryzun33 <yakushevao777@gmail.com>
2023-09-24 19:20:18 +0300

    feat: add function checkfield

--- random-game/index.js
+++ random-game/index.js
@@ -16,6 +16,8 @@ let startPosition = null;
 let activeBall = null;
 let badBoxes = [];
 
+let count = 0;
+
 const directions = {
    toTop: [[0,-1],[-1,0],[1,0],[0,1]],
    toRight: [[1,0],[0,-1],[0,1],[-1,0]],
@@ -55,10 +57,6 @@ function move(direction) {
       break;
     }
   }
-  // if (transArray.length === 0) {
-  //   console.log ('null');
-  //   return;
-  // }
 
   if (startLength === transArray.length) {
     badBoxes.push(transArray[transArray.length - 1]);
@@ -86,6 +84,110 @@ function move(direction) {
   }
 
 }
+
+
+function checkField(ball) {
+  
+  const posBall = ball.getAttribute('data-position');
+  const color = ball.getAttribute('data-color');
+  let diagonal1 = [];
+  let diagonal2 = [];
+  let horizontal = [];
+  let vertical = [];
+
+  // diagonal1
+  if((posBall[0] <= 3 && posBall[1] >=5) || (posBall[0] >=5 && posBall[1] <=3)) {
+    diagonal1 = [];
+  } else {
+    let currPos = posBall;
+    let activeBox = document.getElementById(currPos);
+    diagonal1.push(activeBox);
+    while(+currPos[0] >= 0 && +currPos[1] >=0) {
+      currPos = `${+posBall[0] - 1}` + `${+posBall[1] - 1}`;
+      let box = document.getElementById(currPos);
+      if(box.lastElementChild.matches('.ball') && (box.lastElementChild.dataset.color === color)) {
+        diagonal1.unshift(box);
+      } else {
+        break;
+      }
+    }
+    while(+currPos[0] < numbOfCells && +currPos[1] < numbOfCells) {
+      currPos = `${+posBall[0] + 1}` + `${+posBall[1] + 1}`;
+      let box = document.getElementById(currPos);
+      if(box.lastElementChild.matches('.ball') && (box.lastElementChild.dataset.color === color)) {
+        diagonal1.push(box);
+      } else {
+        break;
+      }
+    }
+  }
+  
+  // diagonal2
+  if((posBall[0] <= 3 && posBall[1] <= 3) || (posBall[0] >=5 && posBall[1] >= 5)) {
+    diagonal2 = [];
+  } else {
+    let currPos = posBall;
+    let activeBox = document.getElementById(currPos);
+    diagonal2.push(activeBox);
+    while(+currPos[0] >= 0 && +currPos[1] <=numbOfCells) {
+      currPos = `${+posBall[0] - 1}` + `${+posBall[1] + 1}`;
+      let box = document.getElementById(currPos);
+      if(box.lastElementChild.matches('.ball') && (box.lastElementChild.dataset.color === color)) {
+        diagonal2.unshift(box);
+      } else {
+        break;
+      }
+    }
+    while(+currPos[0] < numbOfCells && +currPos[1] >=0 ) {
+      currPos = `${+posBall[0] + 1}` + `${+posBall[1] - 1}`;
+      let box = document.getElementById(currPos);
+      if(box.lastElementChild.matches('.ball') && (box.lastElementChild.dataset.color === color)) {
+        diagonal2.push(box);
+      } else {
+        break;
+      }
+    }
+  }
+
+  // horizontal
+  for (let x = 0; x < numbOfCells; x++) {
+    let currPos = x + `${posBall[1]}`;
+    let box = document.getElementById(currPos);
+    if(box.lastElementChild.matches('.ball') && (box.lastElementChild.dataset.color === color)) {
+      horizontal.push(box);
+    } else if (horizontal.length < 5) {
+      horizontal = [];
+    }
+  }
+
+  // vertical
+  for (let y = 0; y < numbOfCells; y++) {
+    let currPos = `${posBall[0]}` + y;
+    let box = document.getElementById(currPos);
+    if(box.lastElementChild.matches('.ball') && (box.lastElementChild.dataset.color === color)) {
+      vertical.push(box);
+    } else if (horizontal.length < 5) {
+      vertical = [];
+    }
+  }
+
+  let arrOfActiveBoxes = [horizontal, vertical, diagonal1, diagonal2];
+  arrOfActiveBoxes.forEach((arr) => {
+    if (arr.length >= 5) {
+      count = count + arr.length;
+      arr.forEach((box) => {
+        let ball = box.lastElementChild;
+        ball.classList.add('hide');
+        ball.addEventListener('transitionend', () => {
+          ball.remove();
+        })
+      })
+    }
+  }) 
+}
+ 
+
+ 
 
 
 // создание контейнера 
@@ -201,11 +303,14 @@ function moveAnimation(ball) {
       ball.classList.remove('endScale');
       transArray = [];
       ball.dataset.position = endPosition;
+      checkField(activeBall);
       activeBall = null;
     })
     
   }, 40 * (transArray.length - 1));
 }
+
+
 
 
       
